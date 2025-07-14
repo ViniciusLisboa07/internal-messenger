e8eed49004564c7f76ec40059b8e208c639fae26
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-07-13 23:08:32 -0300

    feat: define API routes for user authentication and management in v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'authentication#login'
+      post '/logout', to: 'authentication#logout'
+      post '/refresh_token', to: 'authentication#refresh_token'
+      get '/profile', to: 'authentication#profile'
+      put '/profile', to: 'authentication#update_profile'
+      
+      post '/register', to: 'registrations#create'
+      
+      resources :users do
+        member do
+          patch :activate
+          patch :deactivate
+        end
+      end
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
